7719dc251ce6552e9b9121e0b1fa3c18f389f824
Pushkar404 <pushkar8705@gmail.com>
2026-08-22 20:51:07 +0530

    connected backend with frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,27 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [message, setMessage] = useState("");
+  const [courses, setCourses] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/")
-      .then((response) => response.text())
-      .then((data) => setMessage(data))
+    fetch("http://localhost:5000/api/courses")
+      .then((response) => response.json())
+      .then((data) => setCourses(data))
       .catch((error) => console.error(error));
   }, []);
 
   return (
     <div>
       <h1>CAPACITY CONNECT</h1>
-      <p>{message}</p>
+
+      <h2>Available Courses</h2>
+
+      {courses.map((course) => (
+        <div key={course.id}>
+          <h3>{course.title}</h3>
+          <p>{course.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
